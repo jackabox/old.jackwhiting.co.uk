2c9514eddfa01572c78699e7887a2eb67b1c4ba7
Jack Whiting <hi@jackwhiting.co.uk>
2017-10-21 01:54:22 +0100

    minor style tweaks

--- js/main.js
+++ js/main.js
@@ -1,38 +1,38 @@
 $(function () {
 
-	var $body = $('html, body'),
-		$overlay = $('.loading-overlay'),
-		options = {
-			debug: true,
-			forms: '.smoothstate-form',
-			onStart: {
-				// duration: 600,
-				render: function ($container) {
-					$container.addClass('is-exiting');
-					$overlay.fadeIn().addClass('active');
-					smoothState.restartCSSAnimations();
-					$body.animate({ 'scrollTop': 0 });
-				}
-			},
-			onReady: {
-				// duration: 400,
-				render: function ($container, $newContent) {
-					$container.removeClass('is-exiting');		
-					$container.html($newContent);
-				}
-			}, 
-			onAfter: function () {
-				$overlay.fadeOut().removeClass('active');	
-				$('pre code').each(function(i, block) {
-    				hljs.highlightBlock(block);
-  				});					
-			}
-		},
-		smoothState = $('#main').smoothState(options).data('smoothState');
-	
-	setTimeout(function(){
-        $overlay.removeClass('active');
-    }, 1000);
+	// var $body = $('html, body'),
+	// 	$overlay = $('.loading-overlay'),
+	// 	options = {
+	// 		debug: true,
+	// 		forms: '.smoothstate-form',
+	// 		onStart: {
+	// 			// duration: 600,
+	// 			render: function ($container) {
+	// 				$container.addClass('is-exiting');
+	// 				$overlay.fadeIn().addClass('active');
+	// 				smoothState.restartCSSAnimations();
+	// 				$body.animate({ 'scrollTop': 0 });
+	// 			}
+	// 		},
+	// 		onReady: {
+	// 			// duration: 400,
+	// 			render: function ($container, $newContent) {
+	// 				$container.removeClass('is-exiting');
+	// 				$container.html($newContent);
+	// 			}
+	// 		},
+	// 		onAfter: function () {
+	// 			$overlay.fadeOut().removeClass('active');
+	// 			$('pre code').each(function(i, block) {
+    // 				hljs.highlightBlock(block);
+  	// 			});
+	// 		}
+	// 	},
+	// 	smoothState = $('#main').smoothState(options).data('smoothState');
+    //
+	// setTimeout(function(){
+    //     $overlay.removeClass('active');
+    // }, 1000);
 
 	$(document).ready(function() {
 		$('pre code').each(function(i, block) {
